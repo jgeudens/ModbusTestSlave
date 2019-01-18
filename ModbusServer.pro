d073601f870f5bc848d184bade6da238c0444ce5
Jens Geudens <jensgeudens@hotmail.com>
2019-01-18 20:17:08 +0100

    Add version to window title
    Add git information in debug mode

--- ModbusServer.pro
+++ ModbusServer.pro
@@ -1,4 +1,13 @@
 
+VERSION = v1.0.0
+
+DEFINES += DEBUG
+
+DEFINES += GIT_HASH="\\\"$(shell git --git-dir \""$$PWD/.git"\" rev-parse --short HEAD)\\\""
+DEFINES += GIT_BRANCH="\\\"$(shell git --git-dir \""$$PWD/.git"\" rev-parse --abbrev-ref HEAD)\\\""
+
+DEFINES += APP_VERSION=\\\"$$VERSION\\\"
+
 QT       += core gui widgets serialbus
 
 TARGET = ModbusTestSlave
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -72,6 +72,16 @@ MainWindow::MainWindow(QWidget *parent) :
 
     /* Don't stretch columns, resize to contents */
     _pUi->tblRegData->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+
+    QString debugTxt;
+#ifdef DEBUG
+    debugTxt = QString(tr(" beta (git %1:%2)")).arg(GIT_BRANCH).arg(GIT_HASH);
+#endif
+
+    QString windowCaption;
+    windowCaption = QString("ModbusTestSlave %1%2").arg(APP_VERSION).arg(debugTxt);
+
+    setWindowTitle(windowCaption);
 }
 
 MainWindow::~MainWindow()
